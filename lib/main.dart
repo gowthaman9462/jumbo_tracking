7b0e420749f335aa4b7cf3c22c8389fb47524e18
gowthaman <igowthamanks@gmail.com>
2022-07-12 10:00:23 +0530

    task done

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
-
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:sms/sms.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:intl/intl.dart';
+import 'package:telephony/telephony.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-
-
 import 'home.dart';
 import 'model.dart';
 import 'db.dart';
@@ -15,7 +11,51 @@ import 'map.dart';
 import 'user.dart';
 import 'download.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  runApp(MyApp());
+}
+
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+var android = const AndroidInitializationSettings('@mipmap/ic_launcher');
+var iOS = const IOSInitializationSettings();
+var initSetttings = InitializationSettings(android: android, iOS: iOS);
+
+onBackgroundMessage(SmsMessage msg) async {
+  final DbManager dbManager = DbManager();
+  List check;
+  var name;
+  Model model;
+  name = msg.address!.replaceAll("+91", "");
+  check = await dbManager.checkUser(User(name: name));
+  if (check[0]["count(id)"] == 1 &&
+      msg.body!.split(",")[0].trim().toLowerCase() == "jt") {
+    model = Model(
+      name: msg.address!.replaceAll("+91", ""),
+      lat: msg.body!.split(",")[1],
+      lon: msg.body!.split(",")[2],
+      time: DateFormat('yyyy-MM-dd\nHH:mm').format(DateTime.now()),
+    );
+    dbManager.insertModel(model);
+    showNotification(model.name);
+  }
+}
+
+showNotification(String number) async {
+  flutterLocalNotificationsPlugin.initialize(
+    initSetttings,
+    onSelectNotification: (payload) => {main()},
+  );
+  var android = const AndroidNotificationDetails('channel id', 'channel NAME',
+      channelDescription: 'CHANNEL DESCRIPTION',
+      priority: Priority.high,
+      importance: Importance.max);
+  var iOS = const IOSNotificationDetails();
+  var platform = NotificationDetails(android: android, iOS: iOS);
+  await flutterLocalNotificationsPlugin.show(
+      0, 'Jumbo Tracking', 'New Update from $number', platform,
+      payload: '');
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -23,10 +63,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Jumbo Tracking',
-      theme: ThemeData(
-        primarySwatch: Colors.green
-      ),
-      home: const MyHomePage(title:'Jumbo Tracking'),
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: const MyHomePage(title: 'Jumbo Tracking'),
     );
   }
 }
@@ -38,9 +76,7 @@ class MyHomePage extends StatefulWidget {
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
-
 }
-
 
 class _MyHomePageState extends State<MyHomePage> {
   late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
@@ -48,8 +84,9 @@ class _MyHomePageState extends State<MyHomePage> {
   late Model model;
   late List<Model> modelList;
   late Future<List<Model>> list = dbManager.getModelList();
-  void initState(){
-    flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+  final telephony = Telephony.instance;
+  void initState() {
+    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
     var android = const AndroidInitializationSettings('@mipmap/ic_launcher');
     var iOS = const IOSInitializationSettings();
     var initSetttings = InitializationSettings(android: android, iOS: iOS);
@@ -60,79 +97,101 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
   }
 
-  void selectNotification(String? payload){
+  void selectNotification(String? payload) {
     main();
     setState(() {
       _selectedIndex = 1;
     });
   }
 
-  getIncomingMessage() async{
-    SmsReceiver receiver = SmsReceiver();
-    List check;
-    var name;
-    receiver.onSmsReceived.listen((SmsMessage msg) async => {
-      name = msg.address.replaceAll("+91", ""),
-      check = await dbManager.checkUser(User(name: name)),
-      print(name),
-      print(check[0]["count(id)"]),
-      print(msg.body.split(",")[0].trim()),
-      if (check[0]["count(id)"] == 1 && msg.body.split(",")[0].trim().toLowerCase() == "jt"){
-        model = Model(
-          name: msg.address.replaceAll("+91", ""),
-          lat:msg.body.split(",")[1] , 
-          lon:msg.body.split(",")[2], 
-          time:DateFormat('yyyy-MM-dd\nHH:mm').format(msg.date)
-        ),
-        dbManager.insertModel(model),
-        print(msg.address),
-        print(msg.body.split(",")[0].trim().toLowerCase()),
-        print(msg.body.split(",")),
-        print(msg.date),
-        setState(() {
-          list = dbManager.getModelList();
-          _selectedIndex = _selectedIndex;
-        }),
-        setmarker(),
-        showNotification(model.name)
-      }
-    });
+  getIncomingMessage() async {
+    telephony.listenIncomingSms(
+      onNewMessage: onMessage,
+      onBackgroundMessage: onBackgroundMessage,
+    );
   }
 
+  onMessage(SmsMessage msg) async {
+    List check;
+    var name;
+    name = msg.address!.replaceAll("+91", "");
+    check = await dbManager.checkUser(User(name: name));
+    if (check[0]["count(id)"] == 1 &&
+        msg.body!.split(",")[0].trim().toLowerCase() == "jt") {
+      model = Model(
+        name: msg.address!.replaceAll("+91", ""),
+        lat: msg.body!.split(",")[1],
+        lon: msg.body!.split(",")[2],
+        time: DateFormat('yyyy-MM-dd\nHH:mm').format(DateTime.now()),
+      );
+      dbManager.insertModel(model);
+      setState(() {
+        list = dbManager.getModelList();
+        _selectedIndex = _selectedIndex;
+      });
+      setmarker();
+      showNotification(model.name);
+    }
+  }
+
+  // getIncomingMessage() async {
+  //   SmsReceiver receiver = SmsReceiver();
+  //   List check;
+  //   var name;
+  //   receiver.onSmsReceived.listen((SmsMessage msg) async => {
+  //         name = msg.address.replaceAll("+91", ""),
+  //         check = await dbManager.checkUser(User(name: name)),
+  //         print(name),
+  //         print(check[0]["count(id)"]),
+  //         print(msg.body.split(",")[0].trim()),
+  //         if (check[0]["count(id)"] == 1 &&
+  //             msg.body.split(",")[0].trim().toLowerCase() == "jt")
+  //           {
+  //             model = Model(
+  //                 name: msg.address.replaceAll("+91", ""),
+  //                 lat: msg.body.split(",")[1],
+  //                 lon: msg.body.split(",")[2],
+  //                 time: DateFormat('yyyy-MM-dd\nHH:mm').format(msg.date)),
+  //             dbManager.insertModel(model),
+  //             print(msg.address),
+  //             print(msg.body.split(",")[0].trim().toLowerCase()),
+  //             print(msg.body.split(",")),
+  //             print(msg.date),
+  //             setState(() {
+  //               list = dbManager.getModelList();
+  //               _selectedIndex = _selectedIndex;
+  //             }),
+  //             setmarker(),
+  //             showNotification(model.name)
+  //           }
+  //       });
+  // }
+
   showNotification(String number) async {
-    var android = const AndroidNotificationDetails(
-        'channel id', 'channel NAME', 
-        channelDescription:'CHANNEL DESCRIPTION',
+    var android = const AndroidNotificationDetails('channel id', 'channel NAME',
+        channelDescription: 'CHANNEL DESCRIPTION',
         priority: Priority.high,
-        importance: Importance.max
-    );
-    var iOS = const  IOSNotificationDetails();
+        importance: Importance.max);
+    var iOS = const IOSNotificationDetails();
     var platform = NotificationDetails(android: android, iOS: iOS);
     await flutterLocalNotificationsPlugin.show(
         0, 'Jumbo Tracking', 'New Update from $number', platform,
-        payload: 'Nitish Kumar Singh is part time Youtuber');
+        payload: '');
   }
 
   late Set<Marker> _markers1 = {};
-  
-  setmarker() async{
+
+  setmarker() async {
     List<Model> list = await dbManager.getModelList();
+    print(list.length);
     Set<Marker> marker_list = {};
-    for(int i = 0; i< list.length; i++)
-    {
-      print("         ");
-      print(list[i].name);
-      print("          ");
-      marker_list.add(
-        Marker(
-          markerId: MarkerId(list[i].name), 
-          position: LatLng(double.parse(list[i].lat),double.parse(list[i].lon)),
-          infoWindow: InfoWindow(
-            title: list[i].name
-          ),
-          icon: BitmapDescriptor.defaultMarker,
-        )
-      );
+    for (int i = 0; i < list.length; i++) {
+      marker_list.add(Marker(
+        markerId: MarkerId(list[i].name),
+        position: LatLng(double.parse(list[i].lat), double.parse(list[i].lon)),
+        infoWindow: InfoWindow(title: list[i].name),
+        icon: BitmapDescriptor.defaultMarker,
+      ));
       setState(() {
         _markers1 = marker_list;
       });
@@ -140,29 +199,25 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   int _selectedIndex = 0;
-  
+
   void _onItemTapped(int index) {
-    
+    setmarker();
     setState(() {
+      list = dbManager.getModelList();
       _selectedIndex = index;
     });
   }
-  
-
 
   @override
   Widget build(BuildContext context) {
     StatefulWidget widget_;
-    if(_selectedIndex == 0){
+    if (_selectedIndex == 0) {
       widget_ = HomePage(title: "Home", list: list);
-    }
-    else if(_selectedIndex == 1){
+    } else if (_selectedIndex == 1) {
       widget_ = MapPage(list: _markers1);
-    }
-    else if(_selectedIndex == 2){
+    } else if (_selectedIndex == 2) {
       widget_ = const UserPage(title: "Users");
-    }
-    else{
+    } else {
       widget_ = const DownloadPage(title: "Report");
     }
     return Scaffold(
@@ -199,5 +254,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-// flutter run --no-sound-null-safety
